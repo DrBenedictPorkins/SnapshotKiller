c47e68f44235f2cf1fdf3ea6639efcec856373c2
DrPorkins <mike.akram@showtime.com>
2025-02-07 10:48:28 -0500

    Initial Commit

---
+++ ImageMonitor/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ImageMonitor
+//
+//  Created by Dear Null on 2/7/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
